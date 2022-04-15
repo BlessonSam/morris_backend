a5a7a7c9385a972206a40b29908cf1aa113063ad
BlessonSam <69401210+BlessonSam@users.noreply.github.com>
2022-04-15 23:38:54 +0530

    Update urls.py

--- morris_backend/urls.py
+++ morris_backend/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('',include('morris_python.urls'))
 ]
 admin.site.site_header  =  "Administration"  
